746be5649d9a2f36a669735287478587d18ecfde
Daniel <dvbs15@hotmail.com>
2022-07-16 22:41:26 -0300

    Loading Spinner and cursos.json

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,6 +2,7 @@ import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-courses',
@@ -10,7 +11,10 @@ import { HttpClient } from '@angular/common/http'
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+
+  courses$: Observable<Course[]>;
+  //courses: Course[] = [];
+
   displayedColumns = ['name', 'category'];
 
   //coursesService: CoursesService;
@@ -18,11 +22,13 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) {
     //this.courses = [];
     //this.coursesService = new CoursesService();
+    this.courses$ = this.coursesService.list();
 
+    //this.coursesService.list().subscribe(courses => this.courses = courses);
    }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+
   }
 
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs/operators';
 
 import { Course } from '../model/course';
 
@@ -8,11 +9,16 @@ import { Course } from '../model/course';
 })
 export class CoursesService {
 
+  private readonly API ='/assets/cursos.json'
+
   constructor(private httpClient: HttpClient) { }
 
-  list(): Course[] {
-    return [
-      {_id: '1', name: 'Angular', category: 'front-end' }
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(), //como o servidor não é de straming, estamos mais interessados na primeira informação que o servidor enviar, depois encerra a conexão
+      delay(5000),
+      tap( courses => console.log(courses))
+    );
   }
 }
